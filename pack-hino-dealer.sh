6309b644a96d4cabd050e7aa4d3dcbe0d981b9fa
Igor Volkov <igor@aft.ru>
2021-08-05 12:07:46 +0300

    add zipping

--- pack-hino-dealer.sh
+++ pack-hino-dealer.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#todo сделать сайт как переменную
+
 sitePath="/srv/www/hino-dealer.aft.ru/htdocs"
 
 mysqlBackupFileName=$(date '+hino-dealer_aft_ru_%Y-%m-%d_03h00m.%A.sql.gz')
@@ -6,6 +8,7 @@ mysqlBackupFilePathFrom="/var/backups/mysql/daily/hino-dealer_aft_ru/$mysqlBacku
 mysqlBackupFilePathTo="$sitePath/last.sql.gz"
 fileBackupFilePathTo="$sitePath/htdocs.zip"
 
-#rm -rf $mysqlBackupFilePathTo
-#rm -rf $fileBackupFilePathTo
-cp "$mysqlBackupFilePathFrom" "$mysqlBackupFilePathTo"
+rm -rf $mysqlBackupFilePathTo
+rm -rf $fileBackupFilePathTo
+cp $mysqlBackupFilePathFrom $mysqlBackupFilePathTo
+zip -r $fileBackupFilePathTo "sitePath/." -x "upload/tmp*" "upload/logs*" ".git" "bitrix/cache/*" "bitrix/managed_cache/*" "bitrix/backup/*" "bitrix/tmp/*" "bitrix/php_interface/dbconn.php" "bitrix/.settings.php"
