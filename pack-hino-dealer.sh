0fd92f8f15b03c4d43683556d470aad193f83c21
Igor Volkov <igor@aft.ru>
2021-08-05 12:08:32 +0300

    add zipping

--- pack-hino-dealer.sh
+++ pack-hino-dealer.sh
@@ -2,7 +2,6 @@
 #todo сделать сайт как переменную
 
 sitePath="/srv/www/hino-dealer.aft.ru/htdocs"
-
 mysqlBackupFileName=$(date '+hino-dealer_aft_ru_%Y-%m-%d_03h00m.%A.sql.gz')
 mysqlBackupFilePathFrom="/var/backups/mysql/daily/hino-dealer_aft_ru/$mysqlBackupFileName"
 mysqlBackupFilePathTo="$sitePath/last.sql.gz"
