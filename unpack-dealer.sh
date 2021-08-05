146c9cae8621e76f217e53f12f8a59765a3bd740
Igor Volkov <igor@aft.ru>
2021-08-05 15:33:27 +0300

    fix folder paths

--- unpack-dealer.sh
+++ unpack-dealer.sh
@@ -3,8 +3,10 @@
 echo 'Enter site path'
 read sitePath
 
-tmpSitePath="$sitePath/../tmp"
-configSitePath="$sitePath/../config"
+siteParentPath="$(dirname "$sitePath")"
+tmpSitePath="$siteParentPath/tmp"
+
+configSitePath="$siteParentPath/config"
 zippedSitePath="$tmpSitePath/htdocs.zip"
 
 echo 'Enter zip url'
@@ -12,15 +14,6 @@ read backupZipUrl
 
 echo 'Enter http-auth user:'
 read siteAuthUser
-
-echo 'Enter mysql host:'
-read mysqlHost
-
-echo 'Enter mysql user:'
-read mysqlUser
-
-echo 'Enter mysql dbname:'
-read mysqlDBName
 
 rm -rf "$tmpSitePath"
 mkdir "$tmpSitePath"
@@ -31,13 +24,25 @@ if test -f "$zippedSitePath"; then
     mv "$sitePath" "${sitePath}_old"
     mv "${sitePath}_orig" "$sitePath"
     rm -rf "${sitePath}_old"
-    unzip "$zippedSitePath" -d "$tmpSitePath"
+
+    echo 'Unpacking zip...'
+    unzip -qq "$zippedSitePath" -d "$tmpSitePath"
     rm -rf "$tmpSitePath/htdocs.zip"
 
     cp "$configSitePath/dbconn.php" "$tmpSitePath/bitrix/php_interface/dbconn.php"
     cp "$configSitePath/.settings.php" "$tmpSitePath/bitrix/.settings.php"
     cp "$configSitePath/.htaccess" "$tmpSitePath/.htaccess"
     cp "$configSitePath/.htpasswd" "$tmpSitePath/.htpasswd"
+
+    echo 'Enter mysql host:'
+    read mysqlHost
+
+    echo 'Enter mysql user:'
+    read mysqlUser
+
+    echo 'Enter mysql dbname:'
+    read mysqlDBName
+
     zcat "$tmpSitePath/last.sql.gz" | mysql -h"$mysqlHost" -u"$mysqlUser" -p "$mysqlDBName"
 
     mv "$sitePath" "${sitePath}_orig"
